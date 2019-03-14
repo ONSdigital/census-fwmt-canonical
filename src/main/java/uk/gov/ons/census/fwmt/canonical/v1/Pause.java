907441c8d2a373187172c0656bace793c42f4665
jacobharrison <jacob.harrison@qa.com>
2019-03-14 12:01:07 +0000

    added data annotation

--- src/main/java/uk/gov/ons/census/fwmt/canonical/v1/Pause.java
+++ src/main/java/uk/gov/ons/census/fwmt/canonical/v1/Pause.java
@@ -2,10 +2,12 @@ package uk.gov.ons.census.fwmt.canonical.v1;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Pause implements Serializable {
