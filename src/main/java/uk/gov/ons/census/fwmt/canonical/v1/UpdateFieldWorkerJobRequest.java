76a56419ca26081aee890167f2b2acb36484256f
stevescorfield <steve.scorfield@ons.gov.uk>
2019-06-04 17:18:34 +0100

    Update code to reflect true requirements

--- src/main/java/uk/gov/ons/census/fwmt/canonical/v1/UpdateFieldWorkerJobRequest.java
+++ src/main/java/uk/gov/ons/census/fwmt/canonical/v1/UpdateFieldWorkerJobRequest.java
@@ -5,9 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class UpdateFieldWorkerJobRequest {
+  private UUID id;
   private String actionType;
+  private String addressType;
+  private String addressLevel;
+  private Boolean undeliveredAsAddressed;
+  private Boolean blankQreReturned;
+  private OffsetDateTime until;
+  private Boolean ce1Complete;
+  private int ceExpectedResponses;
+  private int ceActualResponses;
 }
